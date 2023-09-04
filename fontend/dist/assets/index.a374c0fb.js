df6a3c8898f621981db2ff23f94368d1734527d3
Sun Zihao <1058907392@qq.com>
2023-09-04 18:03:00 +0800

    add fontend

---
+++ fontend/dist/assets/index.a374c0fb.js
@@ -0,0 +1,1 @@
+import{f as s,v as e,m as t,o}from"./vendor.15027bbd.js";const r={setup:r=>(r,a)=>{const d=t("el-image");return o(),s("div",null,[e(d,{style:{width:"81%"},src:"./index.jpg"})])}};export default r;
